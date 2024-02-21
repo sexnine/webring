d4390e9bd45bebd3f47a56d374da6213cbf88b5f
sexnine <25860388+sexnine@users.noreply.github.com>
2024-02-21 23:18:26 +1100

    add https://revnoplex.github.io to webring

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const sites: Record<string, string> = {
   "0": "https://sexnine.xyz",
   "1": "https://juliapbn.pages.dev",
   "2": "https://mindcool.xyz",
+  "3": "https://revnoplex.github.io",
 };
 
 export const defaultUrl = "https://sexnine.xyz";
