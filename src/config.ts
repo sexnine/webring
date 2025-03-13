0b0842b02c49947e2dac30e21cc3cde786dc055c
sexnine <25860388+sexnine@users.noreply.github.com>
2025-03-13 19:30:16 +1100

    add https://bobbedbob.io to webring

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ export const sites: Record<string, string> = {
   "1": "https://juliapbn.pages.dev",
   "2": "https://mindcool.xyz",
   "3": "https://revnoplex.github.io",
+  "4": "https://bobbedbob.io",
 };
 
 export const defaultUrl = "https://sexnine.xyz";
